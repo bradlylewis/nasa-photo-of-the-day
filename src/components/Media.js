a286f9972f49f5dcdcdcf2d92a22a6380d8ae6e5
BradlyLewis <bradlylewisdev@gmail.com>
2021-07-14 22:53:36 -0400

    finish assignment

--- src/components/Media.js
+++ src/components/Media.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Media({ url }) {
   return  <div className="video-responsive">
     <iframe
-      width="853"
-      height="480"
+      className='media-of-day'
       src={url}
-      frameBorder="0"
       allow="muted; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
